6bf83de1ac49831f15f2db1411145f4456f58305
Sai Pratap <pratap@illiquidlabs.com>
2024-03-18 19:47:21 +0530

    success page and share on x content updated

--- src/pages/mint/MintPageSuccess.tsx
+++ src/pages/mint/MintPageSuccess.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import SubwayPowerVector from '../../components/SubwayPowerVector'
-import { Box } from '@mui/material'
+import React from 'react';
+import styled from 'styled-components';
+import SubwayPowerVector from '../../components/SubwayPowerVector';
+import { Box } from '@mui/material';
 
 const MintPageSuccessContainer = styled.div`
     width: 100%;
@@ -9,7 +9,7 @@ const MintPageSuccessContainer = styled.div`
     height: 100svh;
     box-sizing: border-box;
     background: #000000;
-`
+`;
 
 const Image = styled.img`
     position: absolute;
@@ -17,9 +17,8 @@ const Image = styled.img`
     left: 0;
     width: 100%;
     height: 600px;
-    // object-fit: cover;
     opacity: 0.1;
-`
+`;
 
 const Navbar = styled.nav`
     display: flex;
@@ -39,7 +38,7 @@ const Navbar = styled.nav`
         align-items: center;
         padding: 20px;
     }
-`
+`;
 
 const Li = styled.li`
     text-decoration: none;
@@ -94,7 +93,12 @@ const PassImageWrapper = styled.div`
         height: 50vh;
         max-width: 400px;
     }
-`
+    /* Adjust size of the video container */
+    video {
+        max-width: 40%;
+        max-height: 40vh;
+    }
+`;
 
 const PassContent = styled.div`
     position: absolute;
@@ -109,7 +113,7 @@ const PassContent = styled.div`
     align-items: center;
     justify-content: center;
     z-index: 999;
-`
+`;
 
 const ShareButton = styled.button`
     color: #ffffff;
@@ -139,35 +143,43 @@ const WalletAddress = styled.div`
     }
 `;
 
+const generateTwitterShareUrl = () => {
+    const text = encodeURIComponent("Just minted my Orb Cabal 404 token and joined the ranks of Ponderooors! #etherorb #example");
+    const url = encodeURIComponent("https://example.com"); // Replace with your video URL
+    return `https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=etherorb,example`;
+};
+
 const MintPageSuccess = () => {
-  return (
-    <MintPageSuccessContainer>
-        <Navbar>
-            <SubwayPowerVector
-                group3="/group-3-1.svg"
-                propAlignSelf="unset"
-                propFlexDirection="row"
-                propFlex="unset"
-                propAlignSelf1="stretch"
-            />
+    const twitterShareUrl = generateTwitterShareUrl();
 
-            <Box sx={{
-                display: 'flex',
-                gap: '20px',
-                fontFamily: "var(--font-jetbrains-mono), sans-serif",
-                minWidth: '200px'
-                
-            }}>
-                <Li><a href="https://twitter.com/etherorbxyz" target="_blank" rel="noopener">twitter</a></Li>
-                <Li><a href="https://t.me/EtherOrb404" target="_blank" rel="noopener">telegram</a></Li>
-                <Li><a href="#" className="consto">docs</a></Li>
-                <Li><a href="#" className="consto">PLAY</a></Li>
-            </Box>
-        </Navbar>
-        <Image src="/1cd1b3dbf635d465812b2eb2bea0288e.png" />
+    return (
+        <MintPageSuccessContainer>
+            <Navbar>
+                <SubwayPowerVector
+                    group3="/group-3-1.svg"
+                    propAlignSelf="unset"
+                    propFlexDirection="row"
+                    propFlex="unset"
+                    propAlignSelf1="stretch"
+                />
 
-        <PassImageWrapper>
-        <Box
+                <Box sx={{
+                    display: 'flex',
+                    gap: '20px',
+                    fontFamily: "var(--font-jetbrains-mono), sans-serif",
+                    minWidth: '200px'
+                    
+                }}>
+                    <Li><a href="https://twitter.com/etherorbxyz" target="_blank" rel="noopener">twitter</a></Li>
+                    <Li><a href="https://t.me/EtherOrb404" target="_blank" rel="noopener">telegram</a></Li>
+                    <Li><a href="#" className="consto">docs</a></Li>
+                    <Li><a href="#" className="consto">PLAY</a></Li>
+                </Box>
+            </Navbar>
+            <Image src="/1cd1b3dbf635d465812b2eb2bea0288e.png" />
+
+            <PassImageWrapper>
+                <Box
                     sx={{
                         fontSize: 'var(--font-size-29xl)',
                         fontFamily: 'var(--font-krungthep)',
@@ -181,37 +193,34 @@ const MintPageSuccess = () => {
                 >
                     welcome to the cabal
                 </Box>
-            <video src="/mint.MP4" autoPlay loop muted />
-            <Box
-                sx={{
-                    fontSize: 'var(--font-size-s)',
-                    width: '300px',
-                    margin: '30px',
-                    textAlign: 'center',
-                    fontFamily: 'var(--font-jetbrains-mono)',
-                    color: 'var(--color-white)',
-                    padding: '20px',
-                    textTransform: 'uppercase',
-                    '@media screen and (max-width: 800px)': {
-                        fontSize: 'var(--font-size-xs)'
-                    }
-                }}
-            >
-                This token will grant an airdrop (1:1) of the main token at TGE
-            </Box>
-        </PassImageWrapper> 
+                <video src="/mint.MP4" autoPlay loop muted />
+                <Box
+                    sx={{
+                        fontSize: 'var(--font-size-s)',
+                        width: '300px',
+                        margin: '30px',
+                        textAlign: 'center',
+                        fontFamily: 'var(--font-jetbrains-mono)',
+                        color: 'var(--color-white)',
+                        padding: '20px',
+                        textTransform: 'uppercase',
+                        '@media screen and (max-width: 800px)': {
+                            fontSize: 'var(--font-size-xs)'
+                        }
+                    }}
+                >
+                    orb token will be airdropped upon token launch
+                </Box>
+            </PassImageWrapper> 
 
-        <PassContent>
-            <a
-                href="https://twitter.com/intent/tweet?text=Some%20Custom%20Text&url=https://store-images.s-microsoft.com/image/apps.60673.9007199266244427.4d45042b-d7a5-4a83-be66-97779553b24d.5d82b7eb-9734-4b51-b65d-a0383348ab1b&hashtags=etherorb,example"
-                data-size="large"
-            ><ShareButton >SHARE ON X</ShareButton></a>
-            <WalletAddress>[CONNECTED]</WalletAddress>
-        </PassContent>
+            <PassContent>
+                <a href={twitterShareUrl} data-size="large">
+                    <ShareButton>SHARE ON X</ShareButton>
+                </a>
+                <WalletAddress>[CONNECTED]</WalletAddress>
+            </PassContent>
+        </MintPageSuccessContainer>
+    );
+};
 
-        {/*  onClick={() => window.location.href="https://twitter.com/intent/tweet?text="} */}
-    </MintPageSuccessContainer>
-  )
-}
-
-export default MintPageSuccess
+export default MintPageSuccess;
